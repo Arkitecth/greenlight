32cc71c17ba4761d11a78b0c9bf148cdad924c7f
Trent_T <trenttuedon2000@gmail.com>
2025-02-04 14:28:36 -0500

    Add Error Logic and Validation Package

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,11 +1,34 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unqotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unqotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+	
+	return nil
+}
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 
